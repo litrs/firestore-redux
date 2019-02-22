76586b2a19681a08d71a15fef3f4a136a53b0acf
tewebreklama@gmail.com <tewebreklama@gmail.com>
2019-02-22 15:57:20 +0200

    save project 1

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,15 @@
+const initState = {
+    projects: [
+    {id: '1', title: 'help me find aa', content: 'difdfdf ffgfdfd fddffdd'},
+    {id: '2', title: 'help me find staars', content: 'difdfdf ffgfdfd fddffdd'},
+    {id: '3', title: 'help me find ohooooo', content: 'difdfdf ffgfdfd fddffdd'}
+]
+}
+
+const projectReducer = (state = initState, action) => {
+    
+    return state
+}
+
+
+export default projectReducer
